ae38baa5335a9d04855611028518effac535b8d0
aigecko <aigecko@hotmail.com>
2015-04-11 00:25:58 +0800

    bug fixed

--- POW2.h
+++ POW2.h
@@ -2,12 +2,12 @@
 #define DEF_POW2
 inline bool isPowOf2(int x){
   return (x<0) ?
-    (__builtin_ctz(x)+__builtin_popcount(x))==32&&(x!=-1) :
+    (__builtin_ctz(x)+__builtin_popcount(x))==32&&(~x) :
     (__builtin_popcount(x)==1)&&(x!=1);
 }
 inline bool isPowOf2(long long int x){
   return (x<0) ?
-    (__builtin_ctzll(x)+__builtin_popcountll(x))==32&&(x!=-1) :
+    (__builtin_ctzll(x)+__builtin_popcountll(x))==64&&(~x) :
     (__builtin_popcountll(x)==1)&&(x!=1);
 }
 inline int perfectLog2(int x){
