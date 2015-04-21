4db29b3959870fd12fe2829f103f154fb603e097
aigecko <aigecko@hotmail.com>
2015-04-22 01:56:58 +0800

    POW updated

--- POW2.h
+++ POW2.h
@@ -1,19 +1,27 @@
+#include <stdint.h>
 #ifndef DEF_POW2
 #define DEF_POW2
-inline bool isPowOf2(int x){
+inline bool isPowOf2(uint32_t x){
   return (x<0) ?
-    (__builtin_ctz(x)+__builtin_popcount(x))==32&&(~x) :
+    (__builtin_ctz(x)+__builtin_popcount(x))==sizeof(uint64_t)<<3&&(~x) :
     (__builtin_popcount(x)==1)&&(x!=1);
 }
-inline bool isPowOf2(long long int x){
+inline bool isPowOf2(uint64_t x){
   return (x<0) ?
-    (__builtin_ctzll(x)+__builtin_popcountll(x))==64&&(~x) :
+    (__builtin_ctzll(x)+__builtin_popcountll(x))==sizeof(uint64_t)<<3&&(~x) :
     (__builtin_popcountll(x)==1)&&(x!=1);
 }
-inline int perfectLog2(int x){
+inline bool isPowOf2Positive(uint32_t x){
+  return (__builtin_popcount(x)==1)&&(x!=1);
+}
+inline bool isPowOf2Positive(uint64_t x){
+  return (__builtin_popcountll(x)==1)&&(x!=1);
+}
+inline int perfectLog2(uint32_t x){
   return __builtin_ctz(x);
 }
-inline int perfectLog2(long long int x){
+inline int perfectLog2(uint64_t x){
   return __builtin_ctzll(x);
 }
 #endif
+
