ef479d41c183255694ebe209bbbf7267114eb2dd
aigecko <aigecko@hotmail.com>
2015-04-22 02:09:20 +0800

    POW2.h updated and Readme update

--- POW2.h
+++ POW2.h
@@ -1,26 +1,32 @@
 #include <stdint.h>
 #ifndef DEF_POW2
 #define DEF_POW2
-inline bool isPowOf2(uint32_t x){
+inline bool isPowOf2(int32_t x){
   return (x<0) ?
-    (__builtin_ctz(x)+__builtin_popcount(x))==sizeof(uint64_t)<<3&&(~x) :
+    (__builtin_ctz(x)+__builtin_popcount(x))==sizeof(int32_t)<<3&&(~x) :
     (__builtin_popcount(x)==1)&&(x!=1);
 }
-inline bool isPowOf2(uint64_t x){
+inline bool isPowOf2(int64_t x){
   return (x<0) ?
-    (__builtin_ctzll(x)+__builtin_popcountll(x))==sizeof(uint64_t)<<3&&(~x) :
+    (__builtin_ctzll(x)+__builtin_popcountll(x))==sizeof(int64_t)<<3&&(~x) :
     (__builtin_popcountll(x)==1)&&(x!=1);
 }
-inline bool isPowOf2Positive(uint32_t x){
+inline bool isPowOf2(uint32_t x){
   return (__builtin_popcount(x)==1)&&(x!=1);
 }
-inline bool isPowOf2Positive(uint64_t x){
+inline bool isPowOf2(uint64_t x){
   return (__builtin_popcountll(x)==1)&&(x!=1);
 }
 inline int perfectLog2(uint32_t x){
   return __builtin_ctz(x);
 }
 inline int perfectLog2(uint64_t x){
+  return __builtin_ctzll(x);
+}
+inline int perfectLog2(int32_t x){
+  return __builtin_ctz(x);
+}
+inline int perfectLog2(int64_t x){
   return __builtin_ctzll(x);
 }
 #endif
--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # PowOf2
 Use GCC builtins to deal with power of 2.
-###inline bool isPowOf2(int x)
+###inline bool isPowOf2(int32_t x)
 x can be positive or negative. It will return whether abs(x) is a power number of 2.
-###inline bool isPowOf2(long long int x)
+###inline bool isPowOf2(uint32_t x)
+x is positive only.The same as above.
+###inline bool isPowOf2(int64_t x)
 The same as above, but accept long long int.
-###inline int perfectLog2(int x)
+###inline bool isPowOf2(uint64_t x)
+The same as above, but accept unsigned long long int only.
+###inline int perfectLog2(int32_t x)
+###inline int perfectLog2(uint32_t x)
 x can be only positive. It will return log2(x) if x is a power number of 2.
-###inline int perfectLog2(long long int x)
+###inline int perfectLog2(int64_t x)
+###inline int perfectLog2(uint64_t x)
 The same as above, but accept long long int.
